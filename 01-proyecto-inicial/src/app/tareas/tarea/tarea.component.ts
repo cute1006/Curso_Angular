4e5b5f0b050a4aaaba1740f3fbd83ea308aab034
MariaGuadalupe <mmeza@minsa.gob.ni>
2025-09-04 12:02:13 -0600

    Leccion 4

---
+++ 01-proyecto-inicial/src/app/tareas/tarea/tarea.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+interface Tarea {
+    id: string;
+    idUsuario: string;
+    titulo: string;
+    resumen: string;
+    expira: string;
+}
+@Component({
+  selector: 'app-tarea',
+  standalone: true,
+  imports: [],
+  templateUrl: './tarea.component.html',
+  styleUrl: './tarea.component.css'
+})
+export class TareaComponent {
+@Input({ required: true }) tarea!: Tarea;
+}
